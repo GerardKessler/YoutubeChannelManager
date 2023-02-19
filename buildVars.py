92acf3b1b5019a579e7f99c657d6afba82eaa330
gerardo késsler <gera.kessler@gmail.com>
2023-02-18 21:39:46 -0300

    incluído pathlib.py

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Permite gestionar los canales favoritos de Youtube a través de una interfaz invisible y sencilla"""),
 	# version
-	"addon_version": "1.6",
+	"addon_version": "1.7",
 	# Author(s)
 	"addon_author": "Gerardo Késsler <ReaperYOtrasYerbas@gmail.com>",
 	# URL for the add-on documentation support
